76f0817b5de3563d5855ae6ad8caeaa1f1f8b45e
iftikhar ali <ifti.hazara205@gmail.com>
2025-10-16 10:12:33 +0500

    addignt eh config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          { key: 'X-Frame-Options', value: 'DENY' },
+          { key: 'Content-Security-Policy', value: "frame-ancestors 'none';" },
+        ],
+      },
+    ];
+  },
+};
+
 
 export default nextConfig;
