5077a1b807f2736431c584d99cec00528034e646
iftikhar ali <ifti.hazara205@gmail.com>
2025-10-16 10:05:18 +0500

    complete

--- middleware.js
+++ middleware.js
@@ -1,9 +1,15 @@
-// middleware.js
+// middleware.js (in project root)
 import { NextResponse } from 'next/server';
+
 
 export function middleware(req) {
   const res = NextResponse.next();
+  // Block all iframe embedding
   res.headers.set("X-Frame-Options", "DENY");
   res.headers.set("Content-Security-Policy", "frame-ancestors 'none';");
   return res;
 }
+
+export const config = {
+  matcher: "/:path*", // apply to every route
+};
